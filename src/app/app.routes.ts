10548b29a804ebb55e9fda06d6b06e07df3a39d6
joseca20 <crisskcres1720@gmail.com>
2024-05-28 07:47:50 -0500

    create routes and component login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './feature/auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadChildren: () => import("./feature/feature.module").then(f => f.FeatureModule)},
+    {path: 'autenticacion', loadChildren: () => import("../app/feature/auth/auth.module").then(a => a.AuthModule)},
+    {path: "inicio-sesion", component: LoginComponent}
+];
